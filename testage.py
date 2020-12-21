cc27ae6d76718f2dab77ddb7642b07732e431784
SushilPudke <74047164+SushilPudke@users.noreply.github.com>
2020-12-21 11:15:03 +0530

    Add files via upload

---
+++ testage.py
@@ -0,0 +1,10 @@
+# pro for read name ,age checking for eligibility 
+nm=input("Enter Your Name ")
+age=int(input("Enter Your age"))
+
+print("Name ",nm,"Age",age)
+
+if age>=18:
+    print(nm,"You are eligible to vote")
+else :
+    print(nm,"You are not eligible to vote")
